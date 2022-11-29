4366ebe732944c3451916057f9c559e09fb61f76
GomDiing <gomdiing@gmail.com>
2022-11-29 14:58:33 +0900

    Add/ Create BaseTimeEntity and Set FinalProjectApplication

---
+++ src/main/java/com/kh/finalproject/common/BaseTimeEntity.java
@@ -0,0 +1,27 @@
+package com.kh.finalproject.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+public class BaseTimeEntity {
+    @CreatedDate
+    protected LocalDateTime createdAt;
+
+    @LastModifiedDate
+    protected  LocalDateTime updatedAt;
+}
