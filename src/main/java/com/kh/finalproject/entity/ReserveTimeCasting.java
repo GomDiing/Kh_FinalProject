c8c60d271c4b182e9db16847cb05775ad52282f0
GomDiing <gomdiing@gmail.com>
2022-11-30 06:19:45 +0900

    Modify/ add ReserveTimeCasting.java and Relation with ReserveTimeCasting(@ManyToOne), and Reserve(@OneToMany) ;

---
+++ src/main/java/com/kh/finalproject/entity/ReserveTimeCasting.java
@@ -0,0 +1,19 @@
+package com.kh.finalproject.entity;
+
+import lombok.Getter;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@Table(name = "reserve_time_casting")
+public class ReserveTimeCasting {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "reserve_time_casting_index")
+    private Long index;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "reserve_time_index", nullable = false)
+    private ReserveTime reserveTime;
+}
