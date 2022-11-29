17eb02b446f45f6cf2ad10839bd6e67b571da242
GomDiing <gomdiing@gmail.com>
2022-11-29 16:49:44 +0900

    Add/ add ReviewComment.java and ReviewCommentStatus.java

---
+++ src/main/java/com/kh/finalproject/entity/ReviewComment.java
@@ -0,0 +1,44 @@
+package com.kh.finalproject.entity;
+
+import com.kh.finalproject.common.BaseTimeEntity;
+import lombok.Getter;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@Table(name = "review_comment")
+public class ReviewComment extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "review_comment_index")
+    private Long index;
+
+    @Column(name = "review_title")
+    private String title;
+
+    @Column(name = "review_like")
+    private Integer like;
+
+    @Column(name = "review_rate")
+    private Integer rate;
+
+    @Column(name = "review_comment_content", nullable = false)
+    private String content;
+
+    @Column(name = "review_comment_group", nullable = false)
+    private Long group;
+
+    @Column(name = "review_comment_layer", nullable = false)
+    private Integer layer;
+
+    @Column(name = "review_comment_order", nullable = false)
+    private Integer order;
+
+    @Column(name = "review_comment_status", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private ReviewCommentStatus status;
+
+    @Column(name = "review_comment_accuse_count", nullable = false)
+    private Integer accuseCount;
+}
