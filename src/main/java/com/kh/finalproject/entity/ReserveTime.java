3d489795115fb1bd0c2f8bce867ed535c56a2c6a
GomDiing <gomdiing@gmail.com>
2022-11-29 22:41:33 +0900

    Modify/ add ReserveTime.java and Relation with ReserveTime(@ManyToOne), and Reserve(@OneToMany) ;

---
+++ src/main/java/com/kh/finalproject/entity/ReserveTime.java
@@ -0,0 +1,32 @@
+package com.kh.finalproject.entity;
+
+import lombok.Getter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Entity
+@Table(name = "reserve_time")
+public class ReserveTime {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "reserve_time_index")
+    private Long index;
+
+    @Column(name = "reserve_time_date")
+    private String date;
+
+    @Column(name = "reserve_time_turn")
+    private Integer turn;
+
+    @Column(name = "reserve_time_hour")
+    private Integer hour;
+
+    @Column(name = "reserve_time_min")
+    private Integer minute;
+
+    @OneToMany(mappedBy = "reserveTime")
+    private List<Reserve> reserveList = new ArrayList<>();
+}
