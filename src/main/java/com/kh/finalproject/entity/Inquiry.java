9ced666523415feb7f27bca38302a44885fb6d84
GomDiing <gomdiing@gmail.com>
2022-11-29 16:25:33 +0900

    Add/ Add Inquiry.java and InquiryStatus.java

---
+++ src/main/java/com/kh/finalproject/entity/Inquiry.java
@@ -0,0 +1,38 @@
+package com.kh.finalproject.entity;
+
+import com.kh.finalproject.common.BaseTimeEntity;
+import lombok.Getter;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@Table(name = "inquiry")
+public class Inquiry extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "inquiry_index")
+    private Long index;
+
+    @Column(name = "inquiry_title", nullable = false)
+    private String title;
+
+    @Column(name = "inquiry_category", nullable = false)
+    private String category;
+
+    @Column(name = "inquiry_content", nullable = false)
+    private String content;
+
+    @Column(name = "inquiry_status", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private InquiryStatus status;
+
+    @Column(name = "inquiry_group", nullable = false)
+    private Long group;
+
+    @Column(name = "inquiry_layer", nullable = false)
+    private Integer layer;
+
+    @Column(name = "inquiry_order", nullable = false)
+    private Integer order;
+}
