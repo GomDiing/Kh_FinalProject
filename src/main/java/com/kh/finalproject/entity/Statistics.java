3980bc63df2f9d0ec199f6e2b4c0d21234852924
GomDiing <gomdiing@gmail.com>
2022-11-30 09:18:39 +0900

    Modify/ add Statistics & WishProduct.java and  Relation with WishProduct(@ManyToOne), and Member(@OneToMany)

---
+++ src/main/java/com/kh/finalproject/entity/Statistics.java
@@ -0,0 +1,36 @@
+package com.kh.finalproject.entity;
+
+import lombok.Getter;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@Table(name = "statistics")
+public class Statistics {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "statistics_index")
+    private Long index;
+
+    @Column(name = "statistics_male")
+    private Float male;
+
+    @Column(name = "statistics_female")
+    private Float female;
+
+    @Column(name = "statistics_teen")
+    private Float teen;
+
+    @Column(name = "statistics_twenties")
+    private Float twenties;
+
+    @Column(name = "statistics_thirties")
+    private Float thirties;
+
+    @Column(name = "statistics_forties")
+    private Float forties;
+
+    @Column(name = "statistics_fifties")
+    private Float fifties;
+}
