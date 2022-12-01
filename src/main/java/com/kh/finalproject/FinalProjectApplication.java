747b2ed3b4060ed1c10a38aa299afe9bf82846e0
GomDiing <gomdiing@gmail.com>
2022-12-01 15:17:03 +0900

    Style/ Just For Test Commit Before Merge "Develop" branch

--- src/main/java/com/kh/finalproject/FinalProjectApplication.java
+++ src/main/java/com/kh/finalproject/FinalProjectApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FinalProjectApplication {
-
+	//Just For Test!
 	public static void main(String[] args) {
 		SpringApplication.run(FinalProjectApplication.class, args);
 	}
