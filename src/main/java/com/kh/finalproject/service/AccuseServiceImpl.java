4def791f68812c3374d4ff5803a5b7136d2ec1a5
jimin0601 <jimin600155@naver.com>
2022-12-01 16:42:54 +0900

    This is Test Commit!!!

---
+++ src/main/java/com/kh/finalproject/service/AccuseServiceImpl.java
@@ -0,0 +1,35 @@
+package com.kh.finalproject.service;
+
+import com.kh.finalproject.dto.accuse.AccuseDTO;
+import com.kh.finalproject.dto.accuse.CancelAccuseDTO;
+import com.kh.finalproject.dto.accuse.CreateAccuseDTO;
+import com.kh.finalproject.dto.accuse.ProcessAccuseDTO;
+
+import java.util.List;
+
+public class AccuseServiceImpl implements AccuseService {
+    @Override
+    public void create(CreateAccuseDTO createAccuseDTO) {
+
+    }
+
+    @Override
+    public void process(ProcessAccuseDTO processAccuseDTO) {
+
+    }
+
+    @Override
+    public List<AccuseDTO> searchAll() {
+        return null;
+    }
+
+    @Override
+    public AccuseDTO searchByMemberVictim() {
+        return null;
+    }
+
+    @Override
+    public void cancel(CancelAccuseDTO cancelAccuseDTO) {
+
+    }
+}
