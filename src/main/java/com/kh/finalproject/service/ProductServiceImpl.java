5af7f0605ddb438bd49efdaf8169aa54b5a91bd3
jimin0601 <jimin600155@naver.com>
2022-12-08 12:48:48 +0900

    12.08 jm productServiceImpl update

---
+++ src/main/java/com/kh/finalproject/service/ProductServiceImpl.java
@@ -0,0 +1,24 @@
+package com.kh.finalproject.service;
+
+import com.kh.finalproject.dto.product.BrowseKeywordDTO;
+import com.kh.finalproject.dto.product.DetailProductDTO;
+import com.kh.finalproject.dto.product.ProductDTO;
+
+import java.util.List;
+
+public class ProductServiceImpl implements ProductService {
+    @Override
+    public ProductDTO browseByKeyword(BrowseKeywordDTO browseKeywordDTO) {
+        return null;
+    }
+
+    @Override
+    public List<ProductDTO> searchAll() {
+        return null;
+    }
+
+    @Override
+    public DetailProductDTO detailProductPage(Long productCode) {
+        return null;
+    }
+}
