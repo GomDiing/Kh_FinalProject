68f4278e8aa0fc769ad4487f3c507a40513225a7
GomDiing <gomdiing@gmail.com>
2022-12-09 10:58:01 +0900

    Update/ Update RankStatus.java

--- src/main/java/com/kh/finalproject/entity/enumurate/RankStatus.java
+++ src/main/java/com/kh/finalproject/entity/enumurate/RankStatus.java
@@ -9,7 +9,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum RankStatus {
-    PREPARED("Prepared", "크롤링 대기 중인 상태"),
+    READY("Ready", "크롤링 대기 중인 상태"),
     END("End", "이미 예매가 종료된 상태"),
     SCHEDULED("Scheduled", "아직 예매 대기 중인 상태"),
     COMPLETE("Complete", "정상적으로 크롤링이 만료된 상태");
