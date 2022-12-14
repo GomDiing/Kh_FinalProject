53309af603419f352e1996809a76ed76547733c8
hjm8727 <hjm8727@gmail.com>
2022-12-14 19:14:13 +0900

    kakaopay re

--- src/main/frontend/src/api/AdminApi.js
+++ src/main/frontend/src/api/AdminApi.js
@@ -2,8 +2,8 @@ import axios from "axios";
 import { createSearchParams } from "react-router-dom";
 const HEADER = 'application/json';
 const contentType = 'application/json';
-const TCAT_DOMAIN= "http://localhost:7700";//server path
-const serverPAthTCAT_DOMAIN= "http://localhost:7700";//server path
+//const TCAT_DOMAIN= "http://localhost:7700";//server path
+//const serverPAthTCAT_DOMAIN= "http://localhost:7700";//server path
 // "http://cokebear756.synology.me:33060";
 
 const AdminApi={
@@ -13,20 +13,20 @@ const AdminApi={
             title : inputTitle,
             content : inputDetail
         }
-        return await axios.post(TCAT_DOMAIN + "/notice/write", params, HEADER);
+        return await axios.post("/notice/write", params, HEADER);
     },
     // 공지사항 전체 목록
     noticeInfo : async function(currentPage ,setPageSize){
-        return await axios.get(TCAT_DOMAIN + `/notice/list?page=${(currentPage - 1)}&size=${setPageSize}`, HEADER)
+        return await axios.get(`/notice/list?page=${(currentPage - 1)}&size=${setPageSize}`, HEADER)
     },
     // 공지사항 상세페이지
     noticeDetail : async function(index){
-        return await axios.get(TCAT_DOMAIN + "/notice/detail/" + index, "Text/json")
+        return await axios.get("/notice/detail/" + index, "Text/json")
     },
 
     // 공지사항 삭제 
     noticeDelete : async function(index){
-        return await axios.delete(TCAT_DOMAIN + "/notice/delete/"+ index, HEADER)
+        return await axios.delete("/notice/delete/"+ index, HEADER)
     },
 
     // (체크박스) 공지사항 삭제
@@ -39,7 +39,7 @@ const AdminApi={
             checkDTOList: arrKeys
         };
         // debugger;
-        return await axios.post(TCAT_DOMAIN + "/notice/delete/check",params, "application/json");
+        return await axios.post("/notice/delete/check",params, "application/json");
     },
 
     // 공지사항 수정
@@ -48,16 +48,16 @@ const AdminApi={
             title : inputTitle,
             content : inputDetail
         }
-        return await axios.put(TCAT_DOMAIN + "/notice/edit/" +index, editing, HEADER)
+        return await axios.put("/notice/edit/" +index, editing, HEADER)
     },
 
     // 회원 전체 조회
     totalMember : async function(){
-        return await axios.get(TCAT_DOMAIN + "/memberlist", HEADER)
+        return await axios.get("/memberlist", HEADER)
     },
     // 블랙리스트 회원 조회
     totalBlackMember : async function(){
-        return await axios.get(TCAT_DOMAIN + "/blacklist", HEADER)
+        return await axios.get("/blacklist", HEADER)
     },
 
     // 회원 탈퇴(관리자)
@@ -69,11 +69,11 @@ const AdminApi={
     const params = {
         memberDTOCheckList: arrKeys
     };
-    return await axios.post(TCAT_DOMAIN + "/notice/delete/member/check",params, "application/json");
+    return await axios.post("/notice/delete/member/check",params, "application/json");
 },
     // 일대일문의(qna) 전체 조회
     qnaList : async function(){
-        return await axios.get(TCAT_DOMAIN + "/qna/list", HEADER)
+        return await axios.get("/qna/list", HEADER)
     },
     // qna 관리자 답장
     qnaReply : async function(inputReply,index) {
@@ -82,15 +82,15 @@ const AdminApi={
             reply : inputReply,
             index : index
         }
-        return await axios.post(TCAT_DOMAIN + "/qna/reply", params, HEADER);
+        return await axios.post("/qna/reply", params, HEADER);
     },
     // 차트 정보
     getChart : async function() {
-        return await axios.get(TCAT_DOMAIN + "/admin/chart", HEADER)
+        return await axios.get("/admin/chart", HEADER)
     },
     /* 배너 등록하기(관리자) 아직 미구현   */ 
     uploadBanner : async function(){
-        return await axios.post(TCAT_DOMAIN + "/admin/banner", HEADER)
+        return await axios.post("/admin/banner", HEADER)
     }
 }
 export default AdminApi;
