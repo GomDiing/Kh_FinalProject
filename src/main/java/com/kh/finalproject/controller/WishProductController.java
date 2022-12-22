fb2638c8797f68cd2161e6a3c73271d5353de95e
GomDiing <gomdiing@gmail.com>
2022-12-22 12:20:31 +0900

    추가/ 회원이 찜한 상품 목록 조회 API

--- src/main/java/com/kh/finalproject/controller/WishProductController.java
+++ src/main/java/com/kh/finalproject/controller/WishProductController.java
@@ -2,6 +2,7 @@ package com.kh.finalproject.controller;
 
 import com.kh.finalproject.dto.wishProduct.AddWishProductDTO;
 import com.kh.finalproject.dto.wishProduct.DeleteWishProductDTO;
+import com.kh.finalproject.dto.wishProduct.WishProductDTO;
 import com.kh.finalproject.response.DefaultResponse;
 import com.kh.finalproject.response.DefaultResponseMessage;
 import com.kh.finalproject.response.StatusCode;
@@ -11,6 +12,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -43,6 +46,15 @@ public class WishProductController {
         return new ResponseEntity<>(DefaultResponse.res(StatusCode.OK, DefaultResponseMessage.SUCCESS_CANCEL_WISH_PRODUCT), HttpStatus.OK);
     }
 
-//    @GetMapping("{index}")
-//    public ResponseEntity<DefaultResponse<Object>> searchAllBy
+    /**
+     * 회원 찜목록 조회 컨트롤러
+     * @param index: 회원 인덱스
+     */
+    @GetMapping("{index}")
+    public ResponseEntity<DefaultResponse<Object>> searchAllByMember(@PathVariable Long index) {
+
+        List<WishProductDTO> wishProductDTOList = wishProductService.selectByMember(index);
+
+        return new ResponseEntity<>(DefaultResponse.res(StatusCode.OK, DefaultResponseMessage.SUCCESS_SEARCH_WISH_PRODUCT_BY_MEMBER, wishProductDTOList), HttpStatus.OK);
+    }
 }
