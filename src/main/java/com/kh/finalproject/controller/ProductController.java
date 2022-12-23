d0320521edcfa200e91518b36c96287336e1324d
GomDiing <gomdiing@gmail.com>
2022-12-23 16:11:36 +0900

    수정/ 상품 조회 로직 에러 디버깅, 후기 수정시 평균 평점 변경하도록 수정, 예매 환불 취소에 엔티티 추가, 환불 값 세팅

--- src/main/java/com/kh/finalproject/controller/ProductController.java
+++ src/main/java/com/kh/finalproject/controller/ProductController.java
@@ -32,8 +32,9 @@ public class ProductController {
                                                                        @RequestBody(required = false) DetailRequestDTO requestDTO) {
         //회원 인덱스가 존재하지않으면 -1, 존재하면 해당 값 매핑
         Long isMemberIndex = (long) -1;
-        if (!Objects.isNull(requestDTO.getMemberIndex()))
-            isMemberIndex = requestDTO.getMemberIndex();
+        if (!Objects.isNull(requestDTO))
+            if (!Objects.isNull(requestDTO.getMemberIndex()))
+                isMemberIndex = requestDTO.getMemberIndex();
         DetailProductDTO detailProductDTO = productService.detailProductPage(code, isMemberIndex);
         return new ResponseEntity<>(DefaultResponse.res(StatusCode.OK, DefaultResponseMessage.SUCCESS_SEARCH_PRODUCT_DETAIL, detailProductDTO), HttpStatus.OK);
     }
