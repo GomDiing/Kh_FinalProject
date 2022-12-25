1098309b3cfbf4ea439f9dc9f8f23c52d265869e
Harin Park <rrrin3255@gmail.com>
2022-12-25 16:19:49 +0900

    qna ãshow createTime

--- src/main/frontend/src/components/views/DetailPage/Section/Body/ReviewTest/WriteReview.js
+++ src/main/frontend/src/components/views/DetailPage/Section/Body/ReviewTest/WriteReview.js
@@ -33,20 +33,15 @@ const WriteReview=(props)=>{
           navigate(0);
           } 
         } catch(e){
-          if(e.response.data.statusCode === 400){
-            console.log(e.response.data.errors[0].field);
-            if(e.response.data.code === 'C001'){
-              alert("로그인해라 ㅋ")
-            } else if(e.resonse.data.code === 'M002'){
-              alert("조회한 회원이 없습니다.");
-            } else if(e.response.data.code === 'P001'){
-              alert("해당 상품코드를 참을 수 없습니다.");
-            } else if(e.response.data.code === 'RC002'){
-              alert("평점을 입력 하시기 바랍니다.")
-            }
-          }
-          // else if(e.response.data.code === 'C001'){
-            else{
+          if(e.response.data.code === 'RC002'){
+            alert("평점을 입력하시기 바랍니다.")
+          } else if(e.response.data.code === 'C001'){
+            alert("로그인해라 ㅋ")
+          } else if(e.resonse.data.code === 'M002'){
+            alert("조회한 회원이 없습니다.");
+          } else if(e.response.data.code === 'P001'){
+            alert("해당 상품코드를 참을 수 없습니다.");
+          } else{
             console.log(e);
           }
         }
